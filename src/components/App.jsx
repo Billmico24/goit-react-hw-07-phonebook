770da5ef9df681ee3466a02286e6781a847d0cbe
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-10-11 23:43:56 +0200

    add nanoid

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import React from "react";
+import { nanoid } from 'nanoid'
+
+export class App extends React.Component {
+
+state = {
+  contacts: [ {id: nanoid(5), name: 'Rosie Simpson', number: '459-12-56'},
+              {id: nanoid(5), name: 'Hermione Kline', number: '443-89-12'},
+              {id: nanoid(5), name: 'Eden Clements', number: '645-17-79'},
+              {id: nanoid(5), name: 'Annie Copeland', number: '227-91-26'},],
+  name: ''
+}
+
+   
+
+
+  render() {
+       
+
+    return (
+    <div>
+        <div>main container
+          <h1>Phonebook</h1>
+          <div>add area
+            <div> name input
+              <input
+                  type="text"
+                  name="name"
+                  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                  required
+                />
+            </div>
+            <div>phone input
+                <input
+                  type="tel"
+                  name="number"
+                  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                  required
+                />
+            </div>
+          </div>
+          <div>contacts container
+              <h1>Contacts</h1>
+          </div>
+       </div>
     </div>
   );
+  }
+  
 };
---
+++ src/constants/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+colors: {
+    grayText: "rgb(104, 104, 104)",
+    darkGrey: "rgb(45, 45, 45)",
+    green: "#5CFF00",
+    blue: "#0CCDE8",
+    red: "#FF0D18",
+    yellow: "#F2BF5E"
+    },
+filters: {
+    shadow: "0px 0.7px 3px rgba(0, 0, 0, 0.32)"
+}  
+    
+}
