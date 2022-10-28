4986114e59b1d95650e5da5e88375a8c77b70e8a
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-10-28 15:23:14 +0200

    did contact form with hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,75 +1,79 @@
-import React from "react";
+import {useState} from "react";
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid'
 import { FormContainer, FromInput, SubmitButton } from "./ContactForm.styled";
 
+export function ContactForm({onFormSubmit}) {
 
-export class ContactForm extends React.Component {
-
-
-
-  state = {
-    name: '',
-    number: ''
-    };
-
-  formNameId = nanoid(3);
-  formNumberId = nanoid(3);
-
-
-    handleInputChange = (e) => {
-      const { name, value } = e.currentTarget;
-      
-      this.setState({ [name]: value })
-  };
-
-   handleOnSubmit = e => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  
+ const reset = () => {
+   setName('');
+   setNumber('');
+    }
+const handleOnSubmit = e => {
      e.preventDefault();
      
-       this.props.onFormSubmit(this.state);
-       this.reset();
-   }
-    
-    
-    reset = () => {
-        this.setState({name: '', number: ''})
-    }
+  onFormSubmit({
+    name: name,
+    number: number
+  });
   
-  render() {
+  reset();
+   }
 
-       
+  const handleInputChange = e => {
+    const { name, value } = e.target;
 
-    return (
-    
-        <FormContainer onSubmit={this.handleOnSubmit}>
-            <label htmlFor={this.formNameId} >Name</label>
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      
+      case 'number':
+        setNumber(value);
+        break;
+      
+      default:
+        return;
+    }
+  }
+
+
+  let formNameId = nanoid(3);
+  let formNumberId = nanoid(3);
+
+  return (
+    <FormContainer onSubmit={handleOnSubmit}>
+            <label htmlFor={formNameId}>Name</label>
              <FromInput
-                  onChange={this.handleInputChange}
-                  id={this.formNameId}
+                  id={formNameId}
+                  onChange={handleInputChange}
                   type="text"
                   placeholder="Type name"
-                  value={this.state.name}
+                  value={name}
                   name="name"
                   pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                   title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                   required
-                />
-                <label htmlFor={this.formNumberId} >Number</label>
+      />
+      <label htmlFor={formNumberId} >Number</label>
              <FromInput
-                  onChange={this.handleInputChange}
-                  id={this.formNumberId}
-                  value={this.state.number}
+                  
+                  id={formNumberId}
+                  onChange={handleInputChange}
                   placeholder="Type number"
                   type="tel"
                   name="number"
+                  value={number}
                   pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                   title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                   required
                 />
               <SubmitButton type="submit">Add contact</SubmitButton>
               </FormContainer>
-  );
-  }
+  )
 };
 
 ContactForm.propTypes = {
