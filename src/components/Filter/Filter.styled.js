f8bd029cee1683429be7c326c0e3a3051e534523
Billmico24 <138019098+Billmico24@users.noreply.github.com>
2024-05-03 04:06:37 +0800

    commit all changes

--- src/components/Filter/Filter.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const SearchInput = styled.input`
-    border-top: 1px;
-    border-left: 1px;
-    border-right: 1px;
-    border-width: 1px;
-    background-color: rgb(242, 191, 94, 0);
-`;
--- src/redux/selectors.js
+++
@@ -1,4 +0,0 @@
-export const getContacts = state => state.contacts.items;
-export const getIsLoading = state => state.contacts.isLoading;
-export const getError = state => state.contacts.error;
-export const getFilter = state => state.filter;
